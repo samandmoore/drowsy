ab13ce75c5af3a396ea02a4f8f1ad9842bd3fc50
Sam Moore <samandmoore@gmail.com>
2018-02-03 18:22:47 -0500

    more tests for the other stuff that i care about

--- spec/scoping_spec.rb
+++ spec/scoping_spec.rb
@@ -23,5 +23,20 @@ RSpec.describe 'Scopes' do
       TestUser.scope :with_name, ->(name) { where(name: name) }
       expect(TestUser.with_name('foo').where(something_else: 1).params).to include name: 'foo', something_else: 1
     end
+
+    specify 'scopes defined on child classes do not end up on parent' do
+      define_model('TestChildUser', parent_klass: TestUser, uri: '/child_users{/id}')
+      TestChildUser.scope :with_name, ->(name) { where(name: name) }
+
+      expect(TestChildUser.respond_to?(:with_name)).to eq true
+      expect(TestUser.respond_to?(:with_name)).not_to eq true
+    end
+
+    specify 'scopes are inherited' do
+      define_model('TestChildUser', parent_klass: TestUser, uri: '/child_users{/id}')
+      TestUser.scope :with_name, ->(name) { where(name: name) }
+
+      expect(TestChildUser.respond_to?(:with_name)).to eq true
+    end
   end
 end
